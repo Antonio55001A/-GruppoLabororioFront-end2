53b217ca8ec5bf8550b289e7c13c65cd504c694c
AAA <AAA@BBB.COM>
2022-07-02 13:49:57 +0200

    Home aggiornata (mancano i link)

--- all-files/javascript/homepage.js
+++ all-files/javascript/homepage.js
@@ -1,27 +1,48 @@
 var canzoni = JSON.parse(localStorage.getItem("canzoni"));
-mostra();
-function mostra() {
+mostra("ita");
+mostra("gb");
+mostra("arabi");
+mostraTop();
+function mostra(playlist) {
   if (canzoni != null) {
-    let pagina = document.getElementById("container-stories-reel-stanze");
+    let pagina = document.getElementById(playlist);
     while (pagina.firstChild) {
       pagina.removeChild(pagina.firstChild);
     }
-    for (let i = 0; i < canzoni.length; i++) {
+    let filtroCanzoni = [...canzoni];
+    filtroCanzoni = filtroCanzoni.filter(
+      (canzone) => canzone.playlist === playlist
+    );
+    for (let i = 0; i < filtroCanzoni.length; i++) {
       let riquadro = document.createElement("div");
       riquadro.setAttribute("class", "gruppo");
       riquadro.innerHTML =
-        "<div class='img'><img class='icon-row-menu' src=" +
-        canzoni[i].url +
-        "></div><div <span class='text-storie'>Prezzo: " +
-        canzoni[i].prezzo +
+        "<div class='img'><img class='icon-row-menu' src='" +
+        filtroCanzoni[i].url +
+        "'></div><div <span class='text-storie'>" +
+        filtroCanzoni[i].prezzo +
         "€</span>";
       pagina.appendChild(riquadro);
     }
   }
 }
-
-/*<span class='text-storie'> Nome: " +
-        canzoni[i].canzone +
-        "</span><span class='text-storie'>Artista: " +
-        canzoni[i].artista +
-        "</span>*/
+function mostraTop() {
+  if (canzoni != null) {
+    let copiaCanzoni = [...canzoni];
+    let topCanzoni = [];
+    for (let i = 1; i <= 4; i++) {
+      let indice = Math.floor(Math.random() * copiaCanzoni.length);
+      topCanzoni.push(copiaCanzoni[indice]);
+      copiaCanzoni.splice(indice, 1);
+      let topCanzone = document.getElementById(i);
+      topCanzone.setAttribute("src", topCanzoni[topCanzoni.length - 1].url);
+    }
+  }
+}
+/*
+<span class='text-storie'> Nome: " +
+canzoni[i].canzone +
+"</span><span class='text-storie'>Artista: " +
+canzoni[i].artista +
+"</span>
+*/
